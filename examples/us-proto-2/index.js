6335d62e33955d9628948fd1547265bcf24c9620
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-08 21:15:57 +0100

    docs: add example programs for prototype user stories

--- examples/us-proto-2/index.js
+++ examples/us-proto-2/index.js
@@ -1,1 +1,49 @@
-// TODO
+/**
+ * US-Proto 2
+ * As a package maintainer, I want to take a simple JavaScript
+ * program with JSDoc comments and transform it to TypeScript using
+ * the JSDoc transformer.
+ */
+
+ /**
+  * Left pads the string.
+  * @param {string} str 
+  * @param {number} maxLength 
+  * @returns {string}
+  */
+function padLeft(str, maxLength) {
+  let s = str;
+  while(s.length < maxLength) {
+    s = fill + s;
+  }
+  return s;
+}
+
+/**
+ * Right pads the string.
+ * @param {string} str 
+ * @param {number} maxLength 
+ * @returns {string}
+ */
+function padRight(str, maxLength) {
+  let s = str;
+  while(s.length < maxLength) {
+    s = s + fill;
+  }
+  return s;
+}
+
+/**
+ * Checks if the string is padded.
+ * @param {string} str
+ * @returns {boolean} 
+ */
+function isPadded(str) {
+  return str.length !== str.trim().length;
+}
+
+module.exports = {
+  padLeft,
+  padRight,
+  isPadded
+}
--- examples/us-proto-3/index.js
+++ examples/us-proto-3/index.js
@@ -1,1 +1,32 @@
-// TODO
+/**
+ * US-Proto 3
+ * As a developer interested in migrating to TypeScript, I want to
+ * take a simple JavaScript program that is covered by a test suite,
+ * and transform it to TypeScript using the V8 transformer
+ */
+
+function padLeft(str, maxLength) {
+  let s = str;
+  while(s.length < maxLength) {
+    s = fill + s;
+  }
+  return s;
+}
+
+function padRight(str, maxLength) {
+  let s = str;
+  while(s.length < maxLength) {
+    s = s + fill;
+  }
+  return s;
+}
+
+function isPadded(str) {
+  return str.length !== str.trim().length;
+}
+
+module.exports = {
+  padLeft,
+  padRight,
+  isPadded
+}
